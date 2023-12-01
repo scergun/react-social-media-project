4bc931f18f19feb77caf848afab8a21c585f3347
Samet Çağlar Ergün <sametcaglarergun@gmail.com>
2023-12-01 22:58:43 +0300

    add: HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/main/Home";
 import { Login } from "./pages/Login";
 import { Navbar } from "./components/Navbar";
